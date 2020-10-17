4c2b6b3030aa02e3d506fb0d597cbc95c1c364cb
Lucas Cruz dos Reis <lcr.ergo@gmail.com>
2020-10-17 00:44:52 -0300

    A simple implementation

--- modus_hall.c
+++ modus_hall.c
@@ -13,11 +13,129 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <semaphore.h>
 #include <unistd.h>
+
+int heathens_counter = 0;
+int prudes_counter = 0;
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+sem_t heathens_turn;
+sem_t prudes_turn;
+
+void *
+prudes(void *arg)
+{
+    pthread_mutex_lock(&mutex);
+    sleep(1);
+
+    prudes_counter++;
+    pthread_mutex_unlock(&mutex);
+
+    sleep(1);
+    sem_wait(&prudes_turn);
+    pthread_mutex_lock(&mutex);
+    prudes_counter--;
+
+    if (!prudes_counter && heathens_counter)
+        sem_post(&heathens_turn);
+
+    if (heathens_counter > prudes_counter)
+        sem_post(&heathens_turn);
+    else
+        sem_post(&prudes_turn);
+
+    pthread_mutex_unlock(&mutex);
+
+    return NULL;
+}
+
+void *
+heathens(void *arg)
+{
+    pthread_mutex_lock(&mutex);
+    sleep(1);
+
+    heathens_counter++;
+    pthread_mutex_unlock(&mutex);
+    sleep(1);
+
+    sem_wait(&heathens_turn);
+
+    pthread_mutex_lock(&mutex);
+    heathens_counter--;
+
+    if (!heathens_counter && prudes_counter)
+        sem_post(&prudes_turn);
+
+    if (prudes_counter > heathens_counter)
+        sem_post(&prudes_turn);
+    else
+        sem_post(&heathens_turn);
+
+    pthread_mutex_unlock(&mutex);
+
+    return NULL;
+}
+
+void
+run()
+{
+    int i;
+    int num_heathens, num_prudes, start;
+    pthread_t *tid_prudes, *tid_heathens;
+
+
+    num_heathens = 20;
+    num_prudes = 20;
+
+    if (num_heathens > num_prudes)
+        start = 1;
+    else
+        start = 0;
+
+    tid_heathens = malloc(num_heathens * sizeof(pthread_t));
+    tid_prudes = malloc(num_prudes * sizeof(pthread_t));
+
+    if (sem_init(&heathens_turn, 0, start) < 0) 
+        exit(1);
+
+    if (sem_init(&prudes_turn, 0, !start) < 0) 
+        exit(1);
+
+    if (num_heathens >= num_prudes) {
+        for (i = 0; i < num_prudes; i++) {
+            pthread_create(&tid_prudes[i], NULL, &heathens, NULL);
+            if (i < num_prudes)
+                pthread_create(&tid_prudes[i], NULL, &prudes, NULL);
+        }
+
+
+        for (i = 0; i < num_heathens; i++) {
+            pthread_join(tid_heathens[i], NULL);
+            if (i < num_prudes)
+                pthread_join(tid_prudes[i], NULL);
+        }
+    } else {
+        for(i = 0; i < num_prudes; i++) {
+            pthread_create(&tid_prudes[i], NULL, &prudes, NULL);
+            if (i < num_heathens)
+                pthread_create(&tid_heathens[i], NULL, &heathens, NULL);
+        }
+
+        for (i = 0; i < num_prudes; i++) {
+            pthread_join(tid_prudes[i], NULL);
+            if (i < num_heathens)
+                pthread_join(tid_heathens[i], NULL);
+        }
+    }
+}
 
 int
 main(int argc, char *argv[])
 {
+    run();
 
     return 0;
 }
